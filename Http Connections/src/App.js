065b377033cb05d641925ba899a4c8ddd1a84e5b
Pinglei He <pingleihe@outlook.com>
2022-08-04 16:23:13 +0200

    added functionality to make http requests to the star wars api

--- Http Connections/src/App.js
+++ Http Connections/src/App.js
@@ -1,34 +1,55 @@
-import React from 'react';
-
+import React, { useCallback, useEffect, useState } from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
 
-function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+const checkResponseStatus = (response) => {
+  if (response.ok) return;
+  throw new Error('Oups, something went wrong!');
+};
+
+const IDLE = 'idle';
+const LOADING = 'loading';
+const ERROR = 'error';
+
+const App = () => {
+
+  const [movieList, setMovieList] = useState([]);
+  const [state, setState] = useState(IDLE); // 'idle' || 'loading' || 'error'
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const fetchMoviesHandler = useCallback(async () => {
+    setState(LOADING);
+    try {
+      const response = await fetch('https://swapi.dev/api/films/');
+      checkResponseStatus(response);
+      const data = await response.json();
+      const movies = data.results.map(({ episode_id, title, opening_crawl, release_date }) => ({
+        id: episode_id, title, openingText: opening_crawl, releaseDate: release_date,
+      }));
+      setMovieList(movies);
+      setState(IDLE);
+    } catch (err) {
+      setState(ERROR);
+      setErrorMessage(err.message);
+    }
+  }, [checkResponseStatus]);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, []);
 
   return (
-    <React.Fragment>
-      <section>
-        <button>Fetch Movies</button>
-      </section>
-      <section>
-        <MoviesList movies={dummyMovies} />
-      </section>
-    </React.Fragment>
+      <React.Fragment>
+        <section>
+          <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        </section>
+        <section>
+          {(state === LOADING) ? <p>'Loading...'</p> :
+              (state === ERROR) ? <p>{errorMessage}</p> :
+                  <MoviesList movies={movieList}/>}
+        </section>
+      </React.Fragment>
   );
-}
+};
 
 export default App;
