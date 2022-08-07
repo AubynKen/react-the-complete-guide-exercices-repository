eee210594a1a285e928d0ae17b90e9d37240838d
Pinglei He <pingleihe@outlook.com>
2022-08-07 22:46:46 +0200

    switched to a new form with three inputs

--- Form Input/src/components/SimpleInput.js
+++ Form Input/src/components/SimpleInput.js
@@ -2,27 +2,42 @@ import { useState } from 'react';
 
 const SimpleInput = (props) => {
 
+  /* username state handling */
   const [username, setUsername] = useState('');
   const [isUsernameTouched, setIsUsernameTouched] = useState(false);
 
   const isUsernameValid = username.trim().length !== 0;
   const isUsernameInputValid = isUsernameValid || !isUsernameTouched;
 
-  const usernameChangeHandler = (event) => {
-    setUsername(event.target.value);
-  };
+  const usernameChangeHandler = e => setUsername(e.target.value);
+  const usernameBlurHandler = () => setIsUsernameTouched(true);
 
-  const usernameBlurHandler = (event) => {
-    setIsUsernameTouched(true);
-  };
+  /* email state handling */
+  const [email, setEmail] = useState('');
+  const [isEmailTouched, setIsEmailTouched] = useState(false);
+
+  const isEmailValid = email.includes('@');
+  const isEmailInputValid = isEmailValid || !isEmailTouched;
+
+  const emailChangeHandler = e => setEmail(e.target.value);
+  const emailBlurHandler = () => setIsEmailTouched(true);
+
+  /* overall state handling */
+  const isAllFieldValid = isEmailValid && isUsernameValid;
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
-    setIsUsernameTouched(true);
-    if (!isUsernameValid) return;
+    if (!isAllFieldValid) {
+      setIsUsernameTouched(true);
+      setIsEmailTouched(true);
+      return;
+    }
     setIsUsernameTouched(false);
+    setIsEmailTouched(false);
     setUsername('');
-    console.log(username);
+    setEmail('');
+    console.log('username: ' + username);
+    console.log('email: ' + email);
   };
 
   return (
@@ -32,6 +47,11 @@ const SimpleInput = (props) => {
           <input type="text" value={username} onChange={usernameChangeHandler}
                  onBlur={usernameBlurHandler} id="name"/>
           {!isUsernameInputValid && <p>Username can't be empty!</p>}
+          <label> Email:
+            <input type="email" value={email} onChange={emailChangeHandler}
+                   onBlur={emailBlurHandler} id="email"/>
+          </label>
+          {!isEmailInputValid && <p>Email is not valid!</p>}
         </div>
         <div className="form-actions">
           <button>Submit</button>
