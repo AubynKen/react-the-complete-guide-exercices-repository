e515085e062130db79ff236992bf4737bf2194e7
Pinglei He <pingleihe@outlook.com>
2022-08-24 14:22:13 +0200

    added more testst to the greetings component with toggle button

--- Unit Testing/src/components/Greetings.js
+++ Unit Testing/src/components/Greetings.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+const POST_API_URL = 'https://jsonplaceholder.typicode.com/posts';
 
 const Greetings = () => {
-  return (<h2>Hello, World!</h2>);
+  const [changedText, setChangedText] = useState(false);
+  const [posts, setPosts] = useState([]);
+
+  const clickHandler = () => {
+    setChangedText(true);
+  };
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(POST_API_URL);
+      const data = await response.json();
+      setPosts(data);
+    };
+    fetchPosts().then();
+  }, []);
+
+  return (<div>
+    <h2>Hello, World</h2>
+    {!changedText && <p>Click button to change</p>}
+    {changedText && <p>Greetings!</p>}
+    <button onClick={clickHandler}>Click</button>
+
+    <ul id="posts">
+      {posts.map(el => {
+        return <li key={el.id}>{el.title}</li>;
+      })}
+    </ul>
+  </div>);
 };
 
 export default Greetings;
--- Unit Testing/src/components/Greetings.test.js
+++ Unit Testing/src/components/Greetings.test.js
@@ -1,10 +1,38 @@
 import { render, screen } from '@testing-library/react';
 import Greetings from './Greetings';
+import userEvent from '@testing-library/user-event';
 
 describe("<Greetings />", () => {
   test("renders 'Hello, World'", () => {
     render(<Greetings/>);
-    const matchedElement = screen.getByText(/Hello, World/);
+    const helloWorldElement = screen.getByText(/Hello, World/);
+    expect(helloWorldElement).toBeInTheDocument();
+  })
+
+  test("renders 'click button to change'", () => {
+    render(<Greetings/>);
+    const matchedElement = screen.getByText(/click button to change/i);
     expect(matchedElement).toBeInTheDocument();
+  })
+
+  test("renders 'geetings' after button is clicked", () => {
+    render(<Greetings/>);
+
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+
+    const greetingElement = screen.getByText(/greetings/i);
+    expect(greetingElement).toBeInTheDocument();
+  })
+
+  test("renders not 'click button to change' after button is clicked", () => {
+    render(<Greetings/>);
+
+    // button click
+    const btn = screen.getByRole('button');
+    userEvent.click(btn);
+
+    const matchedElement = screen.queryByText(/click button to change/i);
+    expect(matchedElement).toBeNull();
   })
 })
